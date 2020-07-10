9bcd6535c70f80a1760e2337fc4a976472fbc718
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-07-10 09:06:38 -0700

    Create form custom hook and control the form's stateful logic

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,16 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+export const useForm = (initialValues) => {
+  console.log("Custom form hook is working... it's working!");
+  const [values, setValues] = useState(initialValues);
+
+  const handleChanges = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return [values, handleChanges];
+};
